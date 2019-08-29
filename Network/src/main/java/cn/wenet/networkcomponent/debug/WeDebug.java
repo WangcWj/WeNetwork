74e187fd13eca8c6e3194ec236d2bc0845c5dd61
fr-201902 <wangchaochaoj@163.com>
2019-08-29 19:26:04 +0800

    加了一个debug

--- Network/src/main/java/cn/wenet/networkcomponent/debug/WeDebug.java
+++ Network/src/main/java/cn/wenet/networkcomponent/debug/WeDebug.java
@@ -2,12 +2,22 @@ package cn.wenet.networkcomponent.debug;
 
 import android.util.Log;
 
+/**
+ * create to : 框架的日志打印.
+ *
+ * @author WANG
+ * @date 2019-8-19
+ */
 public class WeDebug {
 
   public static  String TAG = "WeDebug";
 
+  public static boolean DEBUG = true;
+
   public static void e(String message){
-      Log.e(TAG,"WeDebug: "+message+"\n");
+      if(DEBUG) {
+          Log.e(TAG, "WeDebug: " + message + "\n");
+      }
   }
 
 }
