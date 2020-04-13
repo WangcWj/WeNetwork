12007018cbfa83e0ab560af678cbfc2280d348b4
WangChaoChao <wangchaochaoj@163.com>
2020-04-13 17:35:05 +0800

    删除idea

--- app/src/main/java/cn/wang/network/App.java
+++ app/src/main/java/cn/wang/network/App.java
@@ -28,8 +28,6 @@ public class App extends Application {
 
     private void leakCarry() {
         if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
             return;
         }
         LeakCanary.install(this);
