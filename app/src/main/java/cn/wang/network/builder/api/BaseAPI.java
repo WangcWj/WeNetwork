65048037868789b7d8146e605c3175aa51c020e1
WangChaoChao <wangchaochaoj@163.com>
2020-04-14 10:49:02 +0800

    更新

--- app/src/main/java/cn/wang/network/builder/api/BaseAPI.java
+++ app/src/main/java/cn/wang/network/builder/api/BaseAPI.java
@@ -7,14 +7,14 @@ import cn.wenet.networkcomponent.core.Control;
  */
 
 public class BaseAPI {
-    public final static String BASE_URL = "https://******/";
+    public final static String BASE_URL = "https://www.apiopen.top/";
     public final static String BASE_URL_FLAG = Control.DEFAULT_BASE_URL_FLAG;
 
 
-    public final static String WEATHER_BASE_URL = "http://******/";
+    public final static String WEATHER_BASE_URL = "http://apis.juhe.cn/";
     public final static String WEATHER_URL_FLAG = Control.GLOBAL_HEADER + ":weather";
 
 
     public final static String IP_URL_FLAG = Control.GLOBAL_HEADER + ":ip";
-    public final static String IP_BASE_URL = "http://********/";
+    public final static String IP_BASE_URL = "http://121.40.239.162:8098/";
 }
