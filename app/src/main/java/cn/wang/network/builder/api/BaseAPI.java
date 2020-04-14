bd832c7bd2f2d19e6657ffd7e1d86502f895b9ce
WangChaoChao <wangchaochaoj@163.com>
2020-04-14 10:51:10 +0800

    更新

--- app/src/main/java/cn/wang/network/builder/api/BaseAPI.java
+++ app/src/main/java/cn/wang/network/builder/api/BaseAPI.java
@@ -11,10 +11,10 @@ public class BaseAPI {
     public final static String BASE_URL_FLAG = Control.DEFAULT_BASE_URL_FLAG;
 
 
-    public final static String WEATHER_BASE_URL = "http://apis.juhe.cn/";
+    public final static String WEATHER_BASE_URL = "******";
     public final static String WEATHER_URL_FLAG = Control.GLOBAL_HEADER + ":weather";
 
 
     public final static String IP_URL_FLAG = Control.GLOBAL_HEADER + ":ip";
-    public final static String IP_BASE_URL = "http://121.40.239.162:8098/";
+    public final static String IP_BASE_URL = "*******";
 }
